4771c78073a36f190c3b023378fa1f5c4888a447
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-11-03 16:14:44 +0000

    Upload

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, {useState} from 'react';
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+function Semaforo ()  {
+
+	const [rojoBackground, setrojoBackground] = useState("grey");
+	const [amarilloBackground, setamarilloBackground] = useState("grey");
+	const [verdeBackground, setverdeBackground] = useState("grey");
+
+	function rojo () {
+	console.log("rojo");
+	
+	if (rojoBackground === "red"){
+		setrojoBackground ("grey")
+	} else {
+		setrojoBackground ("red")
+		setamarilloBackground ("grey")
+		setverdeBackground ("grey")
+	}
+
+}
+	
+	function amarillo () {
+		console.log("amarillo");
+		
+		if (amarilloBackground === "yellow"){
+			setamarilloBackground ("grey")
+		} else {
+			setamarilloBackground ("yellow")
+			setrojoBackground ("grey")
+			setverdeBackground ("grey")
+		}
+
+	}
+	
+		function verde () {
+			console.log("verde");
+			
+			if (verdeBackground === "green"){
+				setverdeBackground ("grey")
+			} else {
+				setverdeBackground ("green")
+				setrojoBackground ("grey")
+				setamarilloBackground ("grey")
+			}
+		}
+		
+		
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className= " semaforo-container" >
+		<button style={{backgroundColor: rojoBackground, width: 150,
+    height: 150, marginTop:30,borderRadius:100}} onClick={rojo}></button>
+		<button style={{backgroundColor: amarilloBackground, width: 150,
+    height: 150, margin:15, borderRadius:100,}} onClick={amarillo}></button>
+		<button style={{backgroundColor: verdeBackground, width: 150,
+    height: 150, margin:0, borderRadius:100}} onClick={verde}></button>
 		</div>
+		</>
 	);
+
 };
 
-export default Home;
+export default Semaforo;
